988354d479d5fa55bd9a7a23775feeeb7017f913
YoussefBinks25 <youssef.khadhraoui@edu.univ-fcomte.fr>
2024-12-13 13:49:31 +0100

    Réglages du menu

--- src/js/menu.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const sidebar = document.getElementById('sidebar');
-    if (sidebar.style.right === '0px') {
-        sidebar.style.right = '-100%'; // Hide the menu
-    } else {
-        sidebar.style.right = '0px'; // Show the menu
-    }
-}

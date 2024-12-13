8b7d79bec751239fff3010d03a0cc86f71c75ee7
YoussefBinks25 <youssef.khadhraoui@edu.univ-fcomte.fr>
2024-12-13 14:52:15 +0100

    la page découvrez nous

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,8 @@
 function toggleMenu() {
     const sidebar = document.getElementById('sidebar');
     if (sidebar.style.right === '0px') {
-        sidebar.style.right = '-100%'; // Hide menu
+        sidebar.style.right = '-100%'; 
     } else {
-        sidebar.style.right = '0px'; // Show menu
+        sidebar.style.right = '0px'; 
     }
 }
